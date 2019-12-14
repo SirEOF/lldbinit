b68cbc4c5476f855fefb2fefe07500d84c65d96f
Yogesh Swami <axelexic@gmail.com>
2019-12-14 11:59:55 -0800

    Add ability to codesign so that lldb can debug
    Adding ability to codesign from commandline so that lldb doesn't require
    sudo

---
+++ osx_sign_for_debugging.sh
@@ -0,0 +1,58 @@
+KEYCHAIN_ASCCESS=/usr/bin/security
+CODESIGN=/usr/bin/codesign
+
+list_developer_identites() {
+    "$KEYCHAIN_ASCCESS" find-identity -p codesigning -v
+}
+
+codesign_exec( ) {
+
+    if [ "$#" -ne 2 ]
+    then
+	echo "Usage: <executable> <identity>"
+	echo "       Use 'list_developer_identites' command to find developer identities"
+	return 255
+    fi
+
+    exec_name="$1"
+    identity="$2"
+
+    sign_entitlement="<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><dict><key>com.apple.security.get-task-allow</key><true/></dict></plist>"
+
+    $CODESIGN -v $exec_name 2> /dev/null
+
+    if [ "$?" -eq 0 ]
+    then
+	echo "$exec_name is already signed..."
+    else
+	tmpfile=$(mktemp "/tmp/entitlement.XXXXXX")
+	if [ "$?" -ne 0 ]
+	then
+	    echo "Failed to create temporary file... "
+	    return 255
+	fi
+	echo $sign_entitlement > "$tmpfile"
+	echo "Unlocking login keychain"
+	$KEYCHAIN_ASCCESS unlock-keychain
+
+	if [ "$?" -ne 0 ]
+	then
+	    return 254
+	fi
+
+	$CODESIGN -s "$identity" --entitlements "$tmpfile" --timestamp=none "$exec_name"
+	$KEYCHAIN_ASCCESS lock-keychain
+	unlink "$tmpfile"
+    fi
+}
+
+if [ "$#" -eq 2 ]
+then
+    codesign_exec $1 $2
+elif [ "$#" -eq 1 ]
+then
+    list_developer_identites
+else
+    echo "Usage: $0 <exec_name> <identity>  Codesign for debugging"
+    echo "Usage: $0 -l                      List codesigning identities"
+fi
